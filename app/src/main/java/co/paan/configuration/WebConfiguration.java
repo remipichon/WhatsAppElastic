486a8ba3a9635ca85074aedd50bcd8de890f4818
RemiP <Remi.Pichon@pega.com>
2017-09-13 16:39:15 +0200

    [RPI] make it suitable for nginx-proxy

---
+++ app/src/main/java/co/paan/configuration/WebConfiguration.java
@@ -0,0 +1,14 @@
+package co.paan.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/static/index-mail.html");
+    }
+}
--- app/src/main/java/co/paan/controller/HelloControler.java
+++ app/src/main/java/co/paan/controller/HelloControler.java
@@ -1,14 +1,9 @@
 package co.paan.controller;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 
 @RestController
@@ -19,9 +14,4 @@ public class HelloControler {
         return "OKok";
     }
 
-
-    @RequestMapping("/health_")
-    public String health_(@RequestParam(value="name", defaultValue="World") String name) {
-        return "OKokok";
-    }
 }
